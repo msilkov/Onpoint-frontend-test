5f16270a3731fec33cca0d03e3c605816c7fd8ee
Michael <m-silkov@inbox.ru>
2023-04-24 23:10:26 +0300

    feat: add gh deploy

---
+++ onpoint-slider/src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="page__content">
+     <h1>Hello there</h1>
+    </div>
+  );
+}
+
+export default App;
